d77867a53c35ca5d2c874fce9f1796265ceb4e50
N153933 <fride.nordstrand.nilsen@nav.no>
2018-07-17 09:46:23 +0200

    Prøver å overføre admin

---
+++ ezbrev-frontend-v2-web/src/main/web_src/src/components/admin/partials/AdminBrevdata.js
@@ -0,0 +1,73 @@
+import React from 'react';
+import {Button, Col, ListGroupItem, Row} from 'react-bootstrap';
+import * as api from '../../../api';import {bindActionCreators} from "redux";
+import * as adminActions from '~/actions/adminActions';
+import * as adminActionsUtil from '~/actions/adminActionsUtil';
+import {connect} from "react-redux";
+
+class AdminBrevdata extends React.Component {
+    render() {
+        if (this.props.malId in this.props.brevdataList) {
+            return this.props.brevdataList[this.props.malId].map(brevdata => (
+                <ListGroupItem key={brevdata.brevdataId}>
+                    <Row>
+                        <Col sm={3}>
+                            {brevdata.beskrivelse + ' Id: ' + brevdata.brevdataId}
+                        </Col>
+                        <Col sm={3}>
+                            <Button
+                                className={'btn btn-primary'}
+                                bsSize="xsmall"
+                                onClick={() => {
+                                    api.deleteBrevdataExternal(brevdata.brevdataId)
+                                    this.props.actions.deleteBrevdataInternal(brevdata.brevdataId)
+                                }}
+                            >
+                                Slette
+                            </Button>
+                        </Col>
+                        <Col sm={3}>
+                            <Button
+                                className={'btn btn-primary'}
+                                bsSize="xsmall"
+                            >
+                                Rediger maskering
+                            </Button>
+                        </Col>
+                    </Row>
+                </ListGroupItem>
+            ));
+        }
+        else{
+            return null;
+        }
+    }
+}
+
+
+function mapStateToProps(state, ownProps) {
+    return {
+        miljoList: state.admin.adminMiljoList,
+        brevInfo: state.admin.adminBrevInfo,
+        brevpakkeList: state.admin.adminBrevpakkeList,
+        brevmalList: state.admin.adminBrevmalList,
+        miljo: state.admin.adminMiljo,
+        brevpakke: state.admin.adminBrevpakke,
+        brevdataList: state.admin.adminBrevdataList,
+
+    };
+}
+
+function mapDispatchToProps(dispatch) {
+    return {
+        utilActions: bindActionCreators(adminActionsUtil, dispatch),
+        actions: bindActionCreators(adminActions, dispatch),
+
+
+    };
+}
+
+export default connect(
+    mapStateToProps,
+    mapDispatchToProps
+)(AdminBrevdata);
---
+++ ezbrev-frontend-v2-web/src/main/web_src/src/components/admin/partials/AdminTable.js
@@ -0,0 +1,50 @@
+import React from 'react';
+import {bindActionCreators} from 'redux';
+import * as menyValgActions from '~/actions/menyValgActions';
+import * as menyValgActionsUtil from '~/actions/menyValgActionsUtil';
+import {connect} from 'react-redux';
+import AdminTableItem from '~/components/admin/partials/AdminTableItem';
+import * as adminActions from "~/actions/adminActions";
+import * as adminActionsUtil from "~/actions/adminActionsUtil";
+
+class AdminTable extends React.Component {
+    render() {
+        return (
+            <div>
+                <br/>
+                <h4>Brevmaler</h4>
+                {this.props.brevmalList.map(item => (
+                    <AdminTableItem
+                        key={item.malId}
+                        item={item}
+                    />
+                ))}
+            </div>
+        );
+    }
+}
+
+function mapStateToProps(state, ownProps) {
+    return {
+        miljoList: state.admin.adminMiljoList,
+        brevInfo: state.admin.adminBrevInfo,
+        brevpakkeList: state.admin.adminBrevpakkeList,
+        brevmalList: state.admin.adminBrevmalList,
+        miljo: state.admin.adminMiljo,
+        brevpakke: state.admin.adminBrevpakke,
+        brevdataList: state.admin.adminBrevdataList
+    };
+}
+
+function mapDispatchToProps(dispatch) {
+    return {
+        utilActions: bindActionCreators(adminActionsUtil, dispatch),
+        actions: bindActionCreators(adminActions, dispatch)
+
+    };
+}
+
+export default connect(
+    mapStateToProps,
+    mapDispatchToProps
+)(AdminTable);
