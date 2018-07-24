064c39d94702234c56065f03e7b2cb30ea61f301
N153933 <fride.nordstrand.nilsen@nav.no>
2018-07-24 10:56:42 +0200

    grunnlag admoinMaskering

--- ezbrev-frontend-v2-web/src/main/web_src/src/components/admin/partials/AdminMaskPages.js
+++ ezbrev-frontend-v2-web/src/main/web_src/src/components/admin/partials/AdminMaskPages.js
@@ -1,141 +1,39 @@
 import React from 'react';
 import { Button, Col, Grid, Image, Row, Modal } from 'react-bootstrap';
-import { values } from 'lodash';
-import 'jcrop';
-import $ from 'jquery';
 import { Space } from '../../common/scaffolding';
-import adminActions from '~/actions/adminActions';
+import * as adminActions from '~/actions/adminActions';
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
+
 
 class AdminMaskPages extends React.Component {
     constructor(props) {
         super(props);
         this.state = {};
     }
-    //
-    // componentDidMount() {
-    //     this.unMaskStore = MaskStore.listen(() => {
-    //         if (MaskStore.isNew()) {
-    //             this.open();
-    //         }
-    //     });
-    // }
-
-    componentWillUnmount() {
-        this.unMaskStore();
-        this.destroy();
-    }
-
-    destroy() {
-        if (this.state.jcrop_api) {
-            this.state.jcrop_api.destroy();
-        }
-    }
-
-    componentDidUpdate() {
-        const self = this;
-        window.requestAnimationFrame(function() {
-            self.jcrop();
-        });
-    }
-
     changePage(page) {
         this.props.actionsAdmin.setAdminActivePage(page);
         this.setState({ changed: true, active: page });
     }
 
-    // open() {
-    //     this.refs.modal.open();
-    // }
-
-    jcrop() {
-        const { changed } = this.state;
-        const imgHtmlObjExists = !!this.refs.crop;
-        if (!imgHtmlObjExists || !changed) {
-            return;
-        }
-        this.destroy();
-
-        // apply jcrop
-        const self = this;
-        const domNode = this.refs.crop.getDOMNode();
-        let trueWidth = domNode.naturalWidth;
-        let trueHeight = domNode.naturalHeight;
-        let width = domNode.width;
-        let ratio = width / trueWidth;
-        $(domNode).Jcrop(
-            {
-                bgColor: 'grey',
-                trueSize: [trueWidth, trueHeight]
-            },
-            function() {
-                self.setState({ jcrop_api: this, changed: false });
-            }
-        );
-
-        // display masks
-        const { masks, active } = this.state;
-        if (masks[active]) {
-            const $jcrop = $('.jcrop-holder');
-            masks[active].map(mask => {
-                let pos = `width: ${(mask.xslutt - mask.xstart) * ratio}px;
-                    height: ${(mask.yslutt - mask.ystart) * ratio}px;
-                    top: ${mask.ystart * ratio}px;
-                    left: ${mask.xstart * ratio}px;`;
-                $jcrop.append(`<div style='${pos}' class='active-mask'></div>`);
-            });
-        }
-    }
-
     handleCrop() {
-        const { jcrop_api, active } = this.state;
-        const select = jcrop_api.tellSelect();
-        const pageMasks = this.getActivePageMasks();
-        const newMask = {
-            xstart: parseInt(select.x),
-            xslutt: parseInt(select.x2),
-            ystart: parseInt(select.y),
-            yslutt: parseInt(select.y2),
-            sidenr: active
-        };
-        if (
-            newMask.xstart == newMask.xslutt ||
-            newMask.ystart == newMask.yslutt
-        ) {
-            return;
-        }
-        pageMasks.push(newMask);
-        this.setState({ changed: true });
+
     }
 
     getActivePageMasks() {
-        const { masks, active } = this.state;
-        const pageMasks = masks[active] || [];
-        masks[active] = pageMasks;
-        return pageMasks;
+
     }
 
     clearMasks() {
-        this.setState({
-            masks: {},
-            changed: true
-        });
+
     }
 
     saveMasks() {
-        const brevdataId = MaskStore.getBrevdataId();
-        const { masks } = this.state;
-        let masksAsFlatList = values(masks);
-        masksAsFlatList =
-            masksAsFlatList.length > 0
-                ? masksAsFlatList.reduce((a, b) => a.concat(b))
-                : [];
-        ApiActions.postMasksForBrevdata({ masks: masksAsFlatList }, brevdataId);
+
     }
 
     render() {
-        const pages = this.state.admin.pngPages || [];
+        const pages = this.props.pages || [];
         return (
             <Modal show= { this.props.showModal } onHide={this.props.actionsAdmin.setAdminShowModal(false)} title="Maskering" bsSize="large">
                 <Grid>
@@ -165,17 +63,12 @@ class AdminMaskPages extends React.Component {
                         </Col>
                         <Col md={10} style={{ maxWidth: 670 + 'px' }}>
                             <div>
-                                <img
-                                    src={
-                                        'data:image/png;base64,' +
-                                        pages[this.state.active]
-                                    }
+                                <img src={''}
                                     ref="crop"
                                     width="550px"
                                 />
                                 <Button
                                     style={{ width: 15 + '%' }}
-                                    onClick={this.handleCrop.bind(this)}
                                     bsSize="small"
                                     bsStyle="info"
                                 >
@@ -184,7 +77,6 @@ class AdminMaskPages extends React.Component {
                                 <Space />
                                 <Button
                                     style={{ width: 15 + '%' }}
-                                    onClick={this.clearMasks.bind(this)}
                                     bsSize="small"
                                     bsStyle="danger"
                                 >
@@ -193,7 +85,6 @@ class AdminMaskPages extends React.Component {
                                 <Button
                                     style={{ width: 15 + '%' }}
                                     className="pull-right"
-                                    onClick={this.saveMasks.bind(this)}
                                     bsSize="small"
                                     bsStyle="success"
                                 >
@@ -210,7 +101,7 @@ class AdminMaskPages extends React.Component {
 
 function mapStateToProps(state, ownProps) {
     return {
-        page: state.admin.pngPages,
+        pages: state.admin.pngPages,
         //active: state.admin.activePage,
         masks: state.admin.masks,
         showModal: state.admin.showModal
