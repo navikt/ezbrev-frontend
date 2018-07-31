2d19c5b857d7f60a7d03a0e678e5f2377b746e07
Martin Nygård <martin.nygard@nav.no>
2018-07-31 10:05:45 +0200

    Endelig fix på spinner og header

--- ezbrev-frontend-v2-web/src/main/web_src/src/components/common/SpinningWheel.js
+++ ezbrev-frontend-v2-web/src/main/web_src/src/components/common/SpinningWheel.js
@@ -2,17 +2,22 @@ import React from 'react';
 import { connect } from 'react-redux';
 import ReactLoading from 'react-loading';
 
+
 class SpinningWheel extends React.Component {
     render() {
         if (this.props.isLoading) {
-            return <ReactLoading type="spin" />;
+            return (
+                <div className="spinner">
+                <ReactLoading type={"cylon"} height={70} width={70}/>
+                </div>
+            )
         } else {
             return null;
         }
     }
 }
 
-function mapStateToProps(state, ownProps) {
+function mapStateToProps(state) {
     return {
         isLoading: state.loading.isLoading
     };
